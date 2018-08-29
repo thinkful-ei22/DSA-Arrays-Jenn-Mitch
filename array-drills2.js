b5bfd2234b47029719388db81be1361a2d25c059
Jennifer Colna <jennifer.colna@gmail.com>
2018-08-29 14:41:00 -0500

    at the products drill

--- array-drills2.js
+++ array-drills2.js
@@ -33,16 +33,56 @@ function filtering(array){
   console.log('output:', newArray);
 }
 
-function maxSum(array){
-
+function maxSum(array){ //[1,-3, 4, -1]
+  let total=0;
+  let currentTotal=0;
+  for(let i=0; i<=array.length; i++){
+    let num = array[i];
+    currentTotal += num;
+    if(currentTotal > total){ //if current total is larger than total, set total to current total
+      total = currentTotal;
+    }
+  }
+  console.log(total);
 }
 
 function merge(array1, array2){
+  let newArrayIndex = 0;
+  let array1Index = 0;
+  let array2Index = 0;
+  let newArray = [];
+  while (newArrayIndex < (array1.length + array2.length)){
 
+    let value1 = array1[array1Index];
+    let value2 = array2[array2Index];
+
+    if (value1 > value2){
+      newArray[newArrayIndex] = value2;
+      array2Index++;
+    }
+    else{
+      newArray[newArrayIndex] = value1;
+      array1Index++;
+    }
+    newArrayIndex++;
+  }
+  console.log(newArray);
 }
 
 function removeCharacters(string1, string2){
+  let newString = '';
 
+  for (let i = 0; i < string1.length; i++) {
+    let x = string1.charAt(i);
+    for(let j = 0; j< string2.length; j++){
+      let y = string2.charAt(j);
+      if (x===y) {
+        x = '';
+      }
+    }
+    newString += x;
+  }
+  console.log(newString);
 }
 
 function products(array) {
@@ -58,8 +98,8 @@ function stringRotation(str1, str2){
 }
 
 //urlify('i dont know what this does');
-filtering([5, -1, 6, -4, 3, 25, 0]);
-//maxSum([4,6,-3,5,-2,1]);
+// filtering([5, -1, 6, -4, 3, 25, 0]);
+// maxSum([4,6,-3,-1,5,-2,1]);
 //merge([1,3,6,8,11],[2,3,5,8,9,10]);
 //removeCharacters('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou');
 //products([1,3,9,4]);
